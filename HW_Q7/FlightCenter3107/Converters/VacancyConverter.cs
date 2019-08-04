f59098cdc2ec66ec7f31712d8c0f1b2385d8e61f
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-08-04 19:15:22 +0300

    Add files via upload

---
+++ HW_Q7/FlightCenter3107/Converters/VacancyConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace FlightCenter3107
+{
+    public class VacancyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null || value as string == "")
+                return "--";
+
+            int vacancy = (int)value;
+
+            if (vacancy == 0)
+            {
+                return "FULL";
+            }
+            if (vacancy <= 30)
+            {
+                return "ALMOST FULL";
+            }
+
+            return "VACANCY";
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
